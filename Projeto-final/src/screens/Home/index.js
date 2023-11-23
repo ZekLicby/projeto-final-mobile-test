84ef42e5295ef196245582104bd209b1b2e90c2d
Carlos Lins <84579765+ZekLicby@users.noreply.github.com>
2023-11-23 01:42:27 +0000

    Configuração inicial do projeto

---
+++ Projeto-final/src/screens/Home/index.js
@@ -0,0 +1,15 @@
+import { View } from "react-native"
+
+export const Home = () => {
+    return(<View style={styles.container}>
+
+    </View>)
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "red",
+        
+    },
+})
